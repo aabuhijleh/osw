8e65a9ff58aa3be91f1d350db9c86c1d5130e097
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-11-19 20:36:12 +0200

    feat: prompt user to enter MenaME credentials

---
+++ src/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const inquirer = require("inquirer");
+const dateFormat = require("dateformat");
+const getAppDataPath = require("appdata-path");
+const path = require("path");
+const fs = require("fs");
+const makeOSWRequest = require("./request.js");
+
+const configFile = path.join(getAppDataPath(), "osw.json");
+console.log(configFile, makeOSWRequest);
+
+(async () => {
+  const today = () => dateFormat(new Date(), "dd/mm/yyyy");
+  const configFileExists = () => fs.existsSync(configFile);
+  const readValue = (value) => JSON.parse(fs.readFileSync(configFile))[value];
+
+  if (!configFileExists()) {
+    fs.writeFileSync(configFile, "{}");
+  }
+
+  const inputData = await inquirer.prompt([
+    {
+      type: "input",
+      name: "employeeCode",
+      message: "What's your MenaME employee code",
+      when: () => !readValue("employeeCode"),
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "What's your MenaME password",
+      when: () => !readValue("password"),
+    },
+    {
+      type: "input",
+      name: "oswDate",
+      message: "What's the date of this OSW request",
+      default: today,
+    },
+  ]);
+
+  // add values from config file
+  if (!inputData.employeeCode || !inputData.password) {
+    inputData.employeeCode = readValue("employeeCode");
+    inputData.password = readValue("password");
+  }
+
+  fs.writeFileSync(configFile, JSON.stringify(inputData));
+
+  console.log("making the request...");
+
+  await makeOSWRequest(inputData);
+
+  console.log("MenaME OSW request successfully made");
+})();
---
+++ src/request.js
@@ -0,0 +1,52 @@
+const fetch = require("node-fetch");
+
+const request = async (data) => {
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+  // get cookie
+  const loginResponse = await fetch(
+    "https://hr.massar.com/MenaITech/application/hrms/mename/verify_user.php",
+    {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-type": "application/x-www-form-urlencoded",
+        "upgrade-insecure-requests": "1",
+      },
+      body: `employee_code=${data.employeeCode}&password64=${data.password}&password=TUU4enI5&company_code=massar&lang1=1&myaccount=1&language_code=1&submit_melogin_validation=0&submit_login_validation=0&su=&ObjectGUID1=`,
+      method: "POST",
+      mode: "cors",
+    }
+  );
+
+  // clean up cookie
+  const regex1 = /expires=.*?; Max-Age=.*?; path=.*?, /g;
+  const regex2 = /; Path=\/; Domain=.hr.massar.com/g;
+  const cookie = loginResponse.headers
+    .get("set-cookie")
+    .replace(regex1, "")
+    .replace(regex2, "");
+
+  const oswResponse = await fetch(
+    "https://hr.massar.com/MenaITech/application/hrms/mename/ESS/online_request/leave_request1.php",
+    {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-type":
+          "multipart/form-data; boundary=----WebKitFormBoundaryeVrnTnVpAZzkZOxK",
+        "upgrade-insecure-requests": "1",
+        cookie,
+      },
+      body: `------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="leave_date"\r\n\r\n${data.oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="trans_internal_type"\r\n\r\n4\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_hour"\r\n\r\n08\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_min"\r\n\r\n30\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_AmPm"\r\n\r\nAM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_hour"\r\n\r\n05\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_min"\r\n\r\n00\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_AmPm"\r\n\r\nPM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents1"\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="notes"\r\n\r\nRotation\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="transaction_date"\r\n\r\n${data.oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK--\r\n`,
+      method: "POST",
+      mode: "cors",
+    }
+  );
+};
+
+module.exports = request;
