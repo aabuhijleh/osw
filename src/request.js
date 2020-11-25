832af0286f02d305d9cb893a6857dffccf41cea9
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-11-25 13:39:33 +0200

    refactor: destruct request input data

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const btoa = require("btoa");
 const https = require("https");
 
-const request = async (data) => {
+const request = async ({ employeeCode, password, oswDate }) => {
   // workaround MenaME SSL certificate issue
   // see https://stackoverflow.com/questions/20082893/unable-to-verify-leaf-signature
   const agent = new https.Agent({
@@ -21,10 +21,8 @@ const request = async (data) => {
         "content-type": "application/x-www-form-urlencoded",
         "upgrade-insecure-requests": "1",
       },
-      body: `employee_code=${data.employeeCode}&password64=${
-        data.password
-      }&password=${btoa(
-        unescape(encodeURIComponent(data.password))
+      body: `employee_code=${employeeCode}&password64=${password}&password=${btoa(
+        unescape(encodeURIComponent(password))
       )}&company_code=massar&lang1=1&myaccount=1&language_code=1&submit_melogin_validation=0&submit_login_validation=0&su=&ObjectGUID1=`,
       method: "POST",
       mode: "cors",
@@ -53,7 +51,7 @@ const request = async (data) => {
         "upgrade-insecure-requests": "1",
         cookie,
       },
-      body: `------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="leave_date"\r\n\r\n${data.oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="trans_internal_type"\r\n\r\n4\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_hour"\r\n\r\n08\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_min"\r\n\r\n30\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_AmPm"\r\n\r\nAM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_hour"\r\n\r\n05\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_min"\r\n\r\n00\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_AmPm"\r\n\r\nPM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents1"\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="notes"\r\n\r\nRotation\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="transaction_date"\r\n\r\n${data.oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK--\r\n`,
+      body: `------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="leave_date"\r\n\r\n${oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="trans_internal_type"\r\n\r\n4\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_hour"\r\n\r\n08\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_min"\r\n\r\n30\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="f_AmPm"\r\n\r\nAM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_hour"\r\n\r\n05\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_min"\r\n\r\n00\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="t_AmPm"\r\n\r\nPM\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents"; filename=""\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="Support_documents1"\r\n\r\n\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="notes"\r\n\r\nRotation\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK\r\nContent-Disposition: form-data; name="transaction_date"\r\n\r\n${oswDate}\r\n------WebKitFormBoundaryeVrnTnVpAZzkZOxK--\r\n`,
       method: "POST",
       mode: "cors",
       agent,
